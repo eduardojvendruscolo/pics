df4dc428072b40dc7bb103a9284af9ae2c35c58c
Eduardo <eduardo.vendruscolo@codengage.com>
2020-03-15 14:07:41 -0300

    list of images

--- src/Components/ImageList.js
+++ src/Components/ImageList.js
@@ -1,7 +1,9 @@
 import React from "react";
 
-const ImageList = () => {
-  return <div> image list</div>;
+const ImageList = props => {
+  return props.images.map(({ id, description, urls }) => {
+    return <img key={id} alt={description} src={urls.regular} />;
+  });
 };
 
 export default ImageList;
